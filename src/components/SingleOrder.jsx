41cd4c102d382654ebca81973c22d5383237013b
Richard Sulc <sulcric1@fit.cvut.cz>
2022-04-01 12:04:16 +0200

    sfg

--- src/components/SingleOrder.jsx
+++ src/components/SingleOrder.jsx
@@ -201,7 +201,7 @@ const SingleOrder = ({ order, array }) => {
                                                         Other
                                                 </button>
                                         </div>
-                                        <div className="m-2 text-white flex-col items-center justify-center flex-wrap max-h-[40vh] overflow-hidden">
+                                        <div className="m-2 text-white flex flex-col flex-wrap max-h-[40vh] overflow-hidden">
                                                 {array.map((item, index) => {
                                                         return (
                                                                 item.type === category && (
